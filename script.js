06a8e70328953e078856a1f5fa1a4782ea338a99
zyyon1 <zyonvieiram@gmail.com>
2025-03-20 14:19:27 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+});
+
